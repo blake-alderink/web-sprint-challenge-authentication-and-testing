7c99dcd88040ff568768f4932f9a9bf6cf7bdf4d
Blake Alderink <blake.alderink@gmail.com>
2023-03-30 00:44:39 -0700

    added login functinality

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -67,8 +67,36 @@ catch (err) {
   */
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   res.end('implement login, please!');
+
+  try {
+
+    const user = await db('users').where('username', req.body.username).first();
+  
+  //if (exists) { return "this username is already taken"} otherwise continue onwards.
+  if (!req.body.password || !req.body.username) {
+    res.status(404).json({
+      message: 'username and password required'
+    })
+  }
+
+
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.status(200).json({ message: `welcome, ${user.username}`, token: "placeholder"})
+  } else {
+    res.status(404).json({message: "invalid credentials"})
+  }
+
+
+
+}
+
+catch (err) {
+  res.status(500).json({message: err.message})
+}
+
+
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
